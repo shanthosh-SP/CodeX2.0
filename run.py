9ffdb260047e03702785ea4db1831bb47654ebfc
Shanthosh <68721243+shanthosh-SP@users.noreply.github.com>
2023-12-14 13:00:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ import subprocess
 
 
 # Launch Flask app in a separate process
-subprocess.Popen(['/home/appuser/venv/bin/python','app.py'], close_fds=True)
+subprocess.Popen(['python','app.py'], close_fds=True)
 # Streamlit app to upload Excel file and ask questions
 
 st.title("Natural Language to SQL Streamlit App")
